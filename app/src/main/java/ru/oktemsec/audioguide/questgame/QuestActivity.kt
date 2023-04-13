69c4e191e802c07ec72a580bc4362f1d5e538d0e
brearey <brearey4@gmail.com>
2023-04-13 11:00:39 +0900

    # Begin QR codes to print # Added skip button

--- app/src/main/java/ru/oktemsec/audioguide/questgame/QuestActivity.kt
+++ app/src/main/java/ru/oktemsec/audioguide/questgame/QuestActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
@@ -29,34 +30,47 @@ class QuestActivity : AppCompatActivity() {
         val exhibitQuestionTextView: TextView = findViewById(R.id.exhibit_question)
         val qrScannerButton: ImageButton = findViewById(R.id.open_qr_scanner)
         val scoresTextView: TextView = findViewById(R.id.scores)
+        val skipButton: Button = findViewById(R.id.skip_button)
 
         // Game
         game = Game(this)
         game.getNextExhibit()
 
         // Set question to UI
-        updateUI(exhibitQuestionTextView, scoresTextView, game)
+        updateUI(exhibitQuestionTextView, scoresTextView, game, skipButton)
 
         val barcodeLauncher = registerForActivityResult( ScanContract()) { result: ScanIntentResult ->
             if (result.contents == null) {
                 Toast.makeText(this, "Сканирование отменено", Toast.LENGTH_SHORT).show()
             } else {
                 val resultToUser = game.checkAnswer(result.contents)
-                Toast.makeText(this, "$resultToUser \n Ваши очки: ${game.scores}", Toast.LENGTH_LONG).show()
-                game.getNextExhibit()
+                if (resultToUser.contains("Молодец! Правильно")) {
+                    Toast.makeText(this, "$resultToUser \n Ваши очки: ${game.scores}", Toast.LENGTH_LONG).show()
+                    game.getNextExhibit()
+                } else {
+                    Toast.makeText(this, resultToUser, Toast.LENGTH_LONG).show()
+                }
                 // Set question to UI
-                updateUI(exhibitQuestionTextView, scoresTextView, game)
+                updateUI(exhibitQuestionTextView, scoresTextView, game, skipButton)
             }
         }
 
         qrScannerButton.setOnClickListener {
             ScannerHandler.scanCode(barcodeLauncher)
         }
+
+        skipButton.setOnClickListener {
+            game.getNextExhibit()
+            game.scores--
+            updateUI(exhibitQuestionTextView, scoresTextView, game, skipButton)
+        }
     }
 
     @SuppressLint("SetTextI18n")
-    private fun updateUI(questionTextView: TextView, scoresTextView: TextView, game: Game) {
+    private fun updateUI(questionTextView: TextView, scoresTextView: TextView, game: Game, skipButton: Button) {
         questionTextView.text = game.currentExhibit.mystery
         scoresTextView.text = "${getString(R.string.exhibits_count)} ${game.scores}"
+
+        skipButton.isEnabled = game.scores > 0
     }
 }
